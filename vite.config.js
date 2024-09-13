4e45cd604547fa63b0da425b1f535740b9784354
darkearly <ansarinajmul279@gmail.com>
2024-09-13 15:01:02 +0530

    Add all project files to new branch

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Online-Quiz/', // Make sure this matches your GitHub repository name
 })
